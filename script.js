b7df327c4dc9b854523f82bda08813e9fb783c0b
vishal verma <vishal02verma@users.noreply.github.com>
2026-09-19 15:17:45 +0530

    Add portfolio website and bakery project

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 let menuicon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let section = document.querySelector('section');
-let naclinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
-    section.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = section.offsetTop - 150;
-        let height = sec.offsetheight;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
         if (top >= offset && top < offset + height) {
-            navlink.forEach(links => {
-                links.classlist.remove('active');
-                document.querySelector('header nav a[href*= ' + id + ']').classList.add('active')
-            })
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            let activeLink = document.querySelector('header nav a[href="#' + id + '"]');
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
         }
-    })
-}
+    });
+};
 menuicon.onclick = () => {
     menuicon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
